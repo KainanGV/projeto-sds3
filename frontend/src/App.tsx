96d1cc0e0be88c7a44bfb13e00b136cbfb350a32
KainanGV <71570785+KainanGV@users.noreply.github.com>
2021-05-05 21:15:21 -0300

    basic states components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import DataTable from 'components/DataTable';
+import Footer from 'components/Footer';
+import NavBar from 'components/NavBar';
+
 function App() {
   return (
-    <div>
-      <h1 className="bg-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h2>Olá mundo</h2>
+        
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
